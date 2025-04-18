b04fe9451dfb406cc264386a3cbea3171e885585
a.kwiatkowski@consultant.crif.com <a.kwiatkowski@consultant.crif.com>
2025-04-18 17:19:57 +0200

    supabase client consts added

--- src/db/supabase.client.ts
+++ src/db/supabase.client.ts
@@ -6,3 +6,7 @@ const supabaseUrl = import.meta.env.SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.SUPABASE_KEY;
 
 export const supabaseClient = createClient<Database>(supabaseUrl, supabaseAnonKey);
+
+export type SupabaseClient = typeof supabaseClient;
+
+export const DEFAULT_USER_ID = "664f25b7-a243-4e43-b437-800a7bd8a818";
