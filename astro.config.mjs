1136558a7855d3b310c249b7f5aad9ad96e677db
a.kwiatkowski@consultant.crif.com <a.kwiatkowski@consultant.crif.com>
2025-06-03 17:31:13 +0200

    astro.config updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -16,6 +16,7 @@ export default defineConfig({
   server: { port: 3000 },
   vite: {
     plugins: [tailwindcss()],
+    envPrefix: ["SUPABASE_", "PUBLIC_"],
     resolve: {
       alias: import.meta.env.PROD
         ? {
